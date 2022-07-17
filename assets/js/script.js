19f11a5094a85bf7a5f72171a8817ebdd266a387
beatricedaldin <107913447+beatricedaldin@users.noreply.github.com>
2022-07-17 23:09:40 +0200

    quasifinito

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,24 +63,46 @@ breakpoints: {
 
 
   /* ANIMATIONS */
-document.addEventListener('scroll',function(){
 
-    var reveals = document.querySelectorAll(".reveal");
+  const scrollElements = document.querySelectorAll(".reveal");
+
+  const elementInView = (el, dividend = 1) => {
+    const elementTop = el.getBoundingClientRect().top;
   
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = document.body.clientHeight;
-      console.log(windowHeight)
-      var elementTop = reveals[i].getBoundingClientRect().top;
+    return (
+      elementTop <= 500
+    );
+  };
   
-      if(elementTop <= 500){
-        reveals[i].classList.add("active");
-    } else{
-        reveals[i].classList.remove("active");
-       
-    }
-
-    }
-
-
+  const elementOutofView = (el) => {
+    const elementTop = el.getBoundingClientRect().top;
   
-})
+    return (
+      elementTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+  };
+  
+  const displayScrollElement = (element) => {
+    element.classList.add("scrolled");
+  };
+  
+  const hideScrollElement = (element) => {
+    element.classList.remove("scrolled");
+  };
+  
+  const handleScrollAnimation = () => {
+    scrollElements.forEach((el) => {
+      if (elementInView(el, 1.25)) {
+        displayScrollElement(el);
+      } else if (elementOutofView(el)) {
+        hideScrollElement(el)
+      }
+    })
+  }
+  
+  window.addEventListener("scroll", () => { 
+    handleScrollAnimation();
+  });
+
+
+
